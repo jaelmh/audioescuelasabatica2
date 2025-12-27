90b864923321d874c1e7c46d9e3c51f92fba6b0f
Jael MH <jaelmh94@gmail.com>
2025-12-26 23:00:08 -0400

    enero

---
+++ p/crear_htmls_con_contenido_portugues.bat
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+set year=26
+set month=01
+
+for /L %%d in (1,1,31) do (
+    if %%d LSS 10 (
+        set day=0%%d
+    ) else (
+        set day=%%d
+    )
+
+    (
+    echo ^<!DOCTYPE html^>
+    echo ^<html lang="es"^>
+    echo ^<head^>
+    echo ^<meta charset="UTF-8"^> 
+    echo ^<meta name="viewport" content="width=device-width, initial-scale=1.0"^>
+    echo ^<title^>Portugues^</title^>
+    echo ^</head^>
+    echo ^<body^>
+    echo ^<h3^>Ouça o texto diário:^</h3^>
+    echo ^<audio controls controlsList="nodownload" src="!day!!month!!year!.mp3" type="audio/mpeg"^>^</audio^>
+    echo ^</body^>
+    echo ^</html^>
+    ) > !day!!month!!year!.html
+)
+echo 31 archivos HTML creados con el contenido correcto.
+pause
